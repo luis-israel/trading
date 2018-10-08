af1edb936451a1ad52ef724d23f3b6a4cf87080f
luis-israel <lfisrael@gmail.com>
2018-10-08 19:48:09 -0300

    trading tools

--- renko.R
+++ renko.R
@@ -1,6 +1,9 @@
 library(data.table)
 library(xts)
 
+# given a timeseries and a size or a vector of sizes, returns a linear renko
+# bricks timeseries
+
 renko_brick <- function(x, size) {
   ratio <- x / size
   ratio <- floor(ratio)
@@ -14,27 +17,4 @@ renko_brick <- function(x, size) {
   brick <- brick[brick != 0]
   out <- merge(brick, x)
   brick <- out[, 1]
-}
-
-renko_signal <- function(brick, skip, leverage) {
-  if (missing(leverage)) {
-    long <- 1
-    short <- 0
-  }
-  else {
-    long <- leverage[1]
-    short <- leverage[2]
-  }
-  if (missing(skip)) skip <- 1
-  skip <- round(skip)
-  brick_nl <- brick[!is.na(brick)]
-  total <- brick_nl
-  if (skip > 1) for (i in 1:skip) total <- total + shift(brick_nl, i)
-  sig <- ifelse(total >= skip, long, 0)
-  sig <- ifelse(total <= skip * -1, -1, sig)
-  out <- merge(sig, brick)
-  sig <- out[, 1]
-  sig[sig == 0] <- NA
-  sig <- na.locf(sig)
-  sig <- ifelse(sig == -1, short, sig)
 }
